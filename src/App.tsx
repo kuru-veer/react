d7e8f9dd3a71339eeba991492a37a9e32688fd49
rudhra-kuruvella <rudhrak@workathon.us>
2022-06-13 19:59:37 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import IMAGE from './assets/images/logos.png';
+
+
+export default function App() {
+  return (
+    <div>
+      <h1>
+        App hello -
+        {process.env.name}
+      </h1>
+      <img src={IMAGE} alt="" />
+    </div>
+  )
+}
